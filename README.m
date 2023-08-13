7c89cb5f959ab879f801466cd828abfd694d6f42
aisyarafa <142139547+aisyarafa@users.noreply.github.com>
2023-08-13 13:58:02 +0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @aisyarafa
+- 👀 I’m interested in Artificial Intelligence, Machine Learning, and IoT
+- 🌱 I’m currently learning Reinforcement Learning by majoring Electrical Engineering  
+- 💞️ I’m looking to collaborate on Reinforcement Learning
+- 📫 You can reach me by email: aisya.rafa@gmail.com
+
+<!---
+aisyarafa/aisyarafa is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
