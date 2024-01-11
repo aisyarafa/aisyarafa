ec722ff2f45a1748af4c2117fa7334adf81fbc9f
aisyarafa <142139547+aisyarafa@users.noreply.github.com>
2024-01-11 12:54:42 +0700

    Update README.m

--- README.m
+++ README.m
@@ -3,8 +3,3 @@
 - 🌱 I’m currently learning Reinforcement Learning by majoring Electrical Engineering  
 - 💞️ I’m looking to collaborate on Reinforcement Learning
 - 📫 You can reach me by email: aisya.rafa@gmail.com
-
-<!---
-aisyarafa/aisyarafa is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
